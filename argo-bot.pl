9468ef4f9014350c851382cf8e389e0d1b31cb08
Tomas Drbota <argoneuscze@gmail.com>
2012-06-22 11:56:22 +0200

    initial

---
+++ argo-bot.pl
@@ -0,0 +1,75 @@
+# =========
+# COPYRIGHT
+# =========
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2012 Tomas Drbota, contact at argoneuscze@gmail.com
+# For more information see README
+
+# ========
+# INCLUDES
+# ========
+
+#! /usr/bin/perl
+
+use warnings;
+use strict;
+use POE;
+use POE::Component::IRC;
+
+# ===================
+# HELPING SUBROUTINES
+# ===================
+
+# load config file
+sub loadConfig
+{
+	my ($config) = $_[0];
+	open my $inputFile, "<", $config
+		or die "Can't open '$config' - $!";
+	
+	my %configHash;
+		
+	while (<$inputFile>)
+	{
+		chomp;
+		s/^\s+|\s+$|^#.*//;
+		my ($option, $value) = split (/\s*=\s*/, $_, 2);
+		$configHash{$option} = $value;	
+	}	
+	
+	return %configHash;
+}
+
+# ==============
+# CONFIG LOADING
+# ==============
+
+# set default config file
+my $config_file = "argo-bot.cfg";
+
+# let user set alternative config file
+if (@ARGV == 2)
+{
+	if($ARGV[0] eq "-config" && @ARGV == 2) { $config_file = $ARGV[1]; }
+}
+
+# load the config
+my %config = loadConfig($config_file);
+
+# ========
+# BOT CODE
+# ========
+
